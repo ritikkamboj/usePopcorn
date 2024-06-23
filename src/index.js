fd549db70a72dfc80ce16fabf3b3dd20e2ceff1a
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-06-23 01:03:28 -0700

    created the star reusable component, user can pass customize value and validation also applied on those value

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import StarRating from './StarRating';
 // import reportWebVitals from './reportWebVitals';
+function Test()
+{
+  const [movieStars, setMovieStars]= useState(0);
+  return <div>
+    <StarRating color="blue" maxRating={7} onSetRating ={setMovieStars} />
+    <p>The Movie has rated {movieStars} Stars </p>
+  </div>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating maxRating={5} messeges ={['Terrible','Bad','Okay','good','Excellent']}/>
+    <StarRating />
+    <StarRating size={24} maxRating={10} className='text' defaultRating ={3}/>
+    <Test/>
+    {/* we can also pass css class like that  */}
+    {/* <StarRating maxRating={10} />
+    <StarRating  /> */}
   </React.StrictMode>
 );
 
