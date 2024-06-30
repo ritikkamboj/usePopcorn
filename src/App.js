baf8b99c8b304f46ef57817f2bc9ac7d3a23ed63
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-06-30 11:24:37 -0700

    use of useRef to understand that it persist the values on change , but don't re render

--- src/App.js
+++ src/App.js
@@ -250,6 +250,18 @@ function MovieDetail({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
 
+  const countRef =useRef(0);
+
+  useEffect(function(){
+    if(userRating)
+      countRef.current = countRef.current+1;
+
+    
+
+  },[userRating])
+
+
+
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   const watchedUserRating = watched.find(
     (movie) => movie.imdbID === selectedId
@@ -280,6 +292,7 @@ function MovieDetail({ selectedId, onCloseMovie, onAddWatched, watched }) {
       userRating,
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ").at(0)),
+      countRatingDecision : countRef.current
     };
     onAddWatched(newWatchedMovie);
     onCloseMovie();
